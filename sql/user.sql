04402589bf7fc56aaa72c8466d512e1b1b79178e
Xmos <472016475@qq.com>
2018-07-09 00:09:26 +0800

    user表的建表脚本

---
+++ sql/user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `user` (
+  `name` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `age` varchar(255) DEFAULT NULL,
+  `dept` varchar(255) DEFAULT NULL,
+  `level` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
